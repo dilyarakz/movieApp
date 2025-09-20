d2f35a421f5ca3cb7bd75fc72f34dd532e9dd479
Tulegenova, Dilyara <dtulegenova3@gatech.edu>
2025-09-20 01:32:00 -0400

    movie-card is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { Search } from './components/Search'
+import { darkThemeToggleTheme, Spinner } from "flowbite-react";
+import { MovieCard } from './components/MovieCard';
+
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -15,23 +18,33 @@ const API_OPTIONS = {
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
-
+     
       if (!response.ok) {
         throw new Error('Failed to fetch movies');
       }
 
       const data = await response.json();
 
-      console.log(data);
-
+      if (data.response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
     } catch (err) {
       console.log(`Error fetching movies: ${err}`);
       setErrorMessage('Error fetching movies. Please try again later');
+    } finally { 
+      setIsLoading(false)
     }
   }
 
@@ -49,8 +62,19 @@ function App() {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
         <section className='all-movies'>
-          <h2>All Movies</h2>
-          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+          <h2 className='mt-[40px]'>All Movies</h2>
+          {isLoading ? (
+            <Spinner aria-label="Default status example" color='purple'/>
+          ) : errorMessage ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ) : (
+                <ul>
+                  {movieList.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie}/>
+                  ))}
+                </ul>
+            )
+          }
         </section>
         
       </div>
